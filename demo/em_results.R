6461f5b07f945d9ac1be8614716601b22555ed5f
weidewind <weidewind@gmail.com>
2016-06-03 15:23:32 +0300

    test script for perl parallelising

--- demo/em_results.R
+++ demo/em_results.R
@@ -1,8 +1,11 @@
-prot <- "n1"
-categories <- 1
+prot <- "h3"
+categories <- 3
 model <- "weibull" # weibull or exponential 
-#em_output <- parse_em_output(prot, model, categories)
-em_output <- parse_group_output(prot, model, categories)
+em_output <- parse_em_output(prot, model, categories)
+## prints parameters and bic for every em launch
 em_output
 length(em_output)
+## 3d picture of model parameters, produced by all launches of em procedure 
+## prints bics for each launch
+## tells which one is the best
 show_em_all_results(em_output, model)
